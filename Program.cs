61ac18b1fbf9e93c6c0fe5feb3c85fa81fdca343
Evgeniy Kukushkin <eugin86@list.ru>
2022-11-14 23:58:18 +0500

    добавлен код программы

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Начало тестовой задачи");
+
+string[] Arr;
+Console.WriteLine("Введите массив трёх строк через пробел");
+string val = Console.ReadLine();
+Arr = val.Split(' ');
+var result = new string[Arr.Length];
+var realSize = 0;
+foreach (var value in Arr )
+    {
+        if (value.Length <= 3)
+        {
+            result[realSize] = value;
+            realSize++;
+        }
+    }
+Console.WriteLine("Строки с символами равными или менее трёх: ");
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
